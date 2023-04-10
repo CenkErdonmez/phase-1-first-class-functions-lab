34eead4eac3c0c639c61d77eb2248c7632a6b251
Cenk Erdonmez <cenkerdonmez@gmail.com>
2023-04-10 11:32:34 +0300

    Completed assingment

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // Code your solution in this file!
+const returnFirstTwoDrivers = function (array){
+    return([array[0], array[1]])
+}
+
+const  returnLastTwoDrivers = function (array){
+    return([array[array.length-2], array[array.length-1]])
+}
+
+const selectingDrivers =[returnFirstTwoDrivers, returnLastTwoDrivers]
+
+function createFareMultiplier(fare) {
+    return function(num) {
+      return fare * num;
+    };
+  }
+
+  const fareDoubler = createFareMultiplier(2);
+
+  const fareTripler= createFareMultiplier(3);
+
+  function selectDifferentDrivers(array, argument=Function){
+    if (argument === returnFirstTwoDrivers){
+       return returnFirstTwoDrivers(array)
+    }
+    else if (argument= returnLastTwoDrivers){
+        return returnLastTwoDrivers(array)
+    }
+  }
+
+console.log(selectDifferentDrivers(["cenk","arda","deniz","ahmet"], returnFirstTwoDrivers))
